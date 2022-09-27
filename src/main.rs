da4abdf878e8061b02cecc98d01afbb55ff8d871
Complexia <complexia701@gmail.com>
2022-09-27 16:33:36 +1000

    Changed warp to actix-web and added graphql and graphiql routes. Playground works

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,51 @@
-pub mod models;
-pub mod server;
-#[macro_use]
-extern crate juniper_codegen;
-extern crate log;
-extern crate pretty_env_logger;
-#[tokio::main]
-async fn main() {
-    pretty_env_logger::init();
-    println!("GraphQL server running on 4050...");
-    server::start(([127, 0, 0, 1], 4050)).await;
+extern crate juniper;
+
+use std::io;
+use std::sync::Arc;
+
+use actix_web::{web, App, Error, HttpResponse, HttpServer};
+use futures::future::Future;
+use juniper::http::graphiql::graphiql_source;
+use juniper::http::GraphQLRequest;
+
+mod schema;
+
+use crate::schema::{create_schema, Schema};
+
+//start actix-web server on port 8080
+fn main() -> io::Result<()> {
+    let schema = std::sync::Arc::new(create_schema());
+    HttpServer::new(move || {
+        App::new()
+            .data(schema.clone())
+            .service(web::resource("/graphql").route(web::post().to_async(graphql)))
+            .service(web::resource("/graphiql").route(web::get().to(graphiql)))
+    })
+    .bind("localhost:8080")?
+    .run()
+}
+
+// GraphQL endpoint
+fn graphql(
+    st: web::Data<Arc<Schema>>,
+    data: web::Json<GraphQLRequest>,
+) -> impl Future<Item = HttpResponse, Error = Error> {
+    web::block(move || {
+        let res = data.execute(&st, &());
+        Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
+    })
+    .map_err(Error::from)
+    .and_then(|user| {
+        Ok(HttpResponse::Ok()
+            .content_type("application/json")
+            .body(user))
+    })
+}
+
+// GraphiQL playground
+fn graphiql() -> HttpResponse {
+    let html = graphiql_source("http://localhost:8080/graphql");
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(html)
 }
--- src/server/mod.rs
+++
@@ -1,7 +0,0 @@
-use std::net::SocketAddr;
-pub mod routes;
-
-// Starts the server
-pub async fn start(addr: impl Into<SocketAddr>) {
-    warp::serve(routes::make_routes()).run(addr).await;
-}
